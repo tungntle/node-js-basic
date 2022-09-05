dc6e89959d54756a03db7fd5f79a485e544ef204
Lê Nguyễn Thanh Tùng <84010654+tungntle@users.noreply.github.com>
2022-09-05 16:11:47 +0700

    configs

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
+require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080
+
 
 configViewEngine(app)
 
